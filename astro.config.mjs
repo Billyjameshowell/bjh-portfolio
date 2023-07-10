a185cb59e2dd018ab973ca02e7b34506de29d99a
Billy Howell <billyjameshowell@gmail.com>
2023-07-10 00:04:33 -0400

    tried to fix sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import UnoCSS from 'unocss/astro';
 // https://astro.build/config
 export default defineConfig({
   // used to generate images
-  site: process.env.VERCEL_ENV === 'production' ? 'https://brutal.elian.codes/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'https://localhost:3001/',
+  site: process.env.VERCEL_ENV === 'production' ? 'https://billyjameshowell.com/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'https://localhost:3001/',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({
     injectReset: true
